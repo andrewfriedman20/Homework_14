123480b03a8276f478779cbddac92914ed30369f
Andrew Friedman <77944651+andrewfriedman20@users.noreply.github.com>
2021-05-10 16:11:38 -0500

    Establish post model, fields

--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,51 @@
 //Post.js in models dir
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+//This creates model for post, then subsequently creates and defines its reqisite fields
+class Post extends Model {}
+
+
+Post.init(
+    
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1],
+        },
+      },
+      
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id'
+        },
+       
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+     
+      },
+    },
+    {
+       sequelize,
+       freezeTableName: true,
+       underscored: true,
+       modelName: 'post'
+    }
+  
+  );
+  
+  module.exports = Post;
