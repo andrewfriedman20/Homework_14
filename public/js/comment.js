436421ae6b973ad4fc2c88afb69552992195ce35
Andrew Friedman <77944651+andrewfriedman20@users.noreply.github.com>
2021-05-10 17:08:26 -0500

    comment form handler added based on add-post code. still needs work

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,1 +1,34 @@
 //comment.js in public/js dirs
+
+
+const commentFormHandler = async (event) => {
+    event.preventDefault();
+}
+
+  
+    const comment_content = document.querySelector('input[name="comment-body"]').value.trim();
+   
+// FIGURE OUT THE POST ID CONST HERE
+//     const post_id = document.querySelector('input[name="post-id"]').value.trim();
+  
+
+    const response = await fetch('/api/comments', {
+        method: 'POST',
+        body: JSON.stringify({ post_id, comment_content }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+  
+      if (response.ok) {
+        document.location.reload();
+      } else {
+        alert(response.statusText);
+      }
+
+
+  
+  
+  document
+    .querySelector('.comment-form')
+    .addEventListener('submit', commentFormHandler);
+  
