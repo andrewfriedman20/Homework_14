5b40e9995b4b1038c55a2b5676621106cf85e343
Andrew Friedman <77944651+andrewfriedman20@users.noreply.github.com>
2021-05-10 15:58:43 -0500

    overhaul of comment model for post, user id, etc

--- models/Comment.js
+++ models/Comment.js
@@ -1,1 +1,58 @@
 //Comment.js in models dir
+
+const { Model, DataTypes } = require('sequelize');
+
+
+const sequelize = require('../config/connection');
+
+
+class User extends Model {}
+
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    comment_content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+            validate: {
+            len[1],
+    },
+
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len[1]},
+      references: {
+        key: 'id',
+        model:'user'
+      },
+
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        key: 'id',
+        model:'post'
+      },
+    },
+  },
+
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+
+);
+
+module.exports = Comment;
