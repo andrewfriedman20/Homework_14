de4389a12ea38f02de110e39c931803d9a1b4440
andrewfriedman <andrewfriedman20@gmail.com>
2021-05-10 23:46:38 -0500

    controller/api/index.js const routes and use updated

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,14 @@
 const router = require('express').Router();
+
 const userRoutes = require('./userRoutes');
+const commentRoutes = require('./commentRoutes');
+const postRoutes = require('./postRoutes');
+
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
+
+
 
 module.exports = router;
