3d482c619ee91e3a55d1f934071ea0220713395c
Andrew Friedman <77944651+andrewfriedman20@users.noreply.github.com>
2021-05-10 23:34:05 -0500

    update consts commentRoutes, router get

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,1 +1,16 @@
 //commentRoutes.js in controllers/api dirs
+
+const router = require('express').Router();
+
+const { Comment } = require('../../models');
+
+const withAuth = require('../../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+      Comment.findAll()
+        .then((CommentInfodb) => res.json(CommentInfodb))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
