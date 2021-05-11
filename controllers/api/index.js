e7df1490beb9ed6ad2b9471f210a5f671644ce88
andrewfriedman <andrewfriedman20@gmail.com>
2021-05-10 23:27:57 -0500

    update consts in controllers/api/index.js UNDONE

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const { Comment } = require('../../models');
-const withAuth = require('../../utils/auth');
 
-
-// router.use('/users', userRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
